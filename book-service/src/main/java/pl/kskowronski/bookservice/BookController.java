c2aecf8e40eb2dfb3950640477c42a6b6d552fb3
k.skowronski <claude-plos@o2.pl>
2024-11-11 22:43:47 +0100

    html

--- book-service/src/main/java/pl/kskowronski/bookservice/BookController.java
+++ book-service/src/main/java/pl/kskowronski/bookservice/BookController.java
@@ -1,12 +1,17 @@
 package pl.kskowronski.bookservice;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/book-service")
+@CrossOrigin("*")
 public class BookController {
 
     @Value("${eureka.instance.instance-id}")
@@ -16,4 +21,14 @@ public class BookController {
     public String get() {
         return "Hello World. my id is " + id;
     }
+
+    @GetMapping("/get-ebooks")
+    public List<Ebook> getEbooks() {
+        List<Ebook> ebooks = List.of(
+                new Ebook("Book Spring Live 1 ", "10$", "Klaudiusz Skowronski", "https://assets.blurb.com/pages/website-assets/lp-homepage/3_Tradebooks-922752db04177f3417c8505ff1970f9d88be19f966cff7ce4654bd85c5073ac3.png", "")
+        , new Ebook("Book Java Live 2 ", "20$", "Klaudiusz Skowronski", "https://pngimg.com/d/book_PNG2111.png", "")
+        );
+        return ebooks;
+    }
+
 }
---
+++ book-service/src/main/java/pl/kskowronski/bookservice/Ebook.java
@@ -0,0 +1,4 @@
+package pl.kskowronski.bookservice;
+
+public record Ebook(String info, String price, String author, String imageUrl, String purchaseUrl) {
+}
